5d93c683312fa4b01e96f67e8ab4a8327323edc0
Lion-D-Yovo <lion.abonnements@gmail.com>
2018-07-11 18:16:28 +0200

    commit 2

---
+++ bot_twitter.rb
@@ -0,0 +1,13 @@
+require 'dotenv'
+require 'twitter'
+
+Dotenv.load
+
+# quelques lignes qui enregistrent les clés d'APIs
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_API_KEY"]
+  config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+  config.access_token        = ENV["TWITTER_API_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_API_ACCESS_TOKEN_SECRET"]
+end
+p client
